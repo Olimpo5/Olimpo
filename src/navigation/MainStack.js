12cac8c4756c09ac9971c210f31a175b3ce024d5
Luispmv <luispmv07@hotmail.com>
2025-10-19 12:55:45 -0600

    Adding Olimpo's SplashScreen

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen";
+import SplashScreen from "../screens/SplashScreen";
 
 const {createNativeStackNavigator} = require("@react-navigation/native-stack")
 
@@ -13,6 +14,7 @@ export default function MainStack(){
             <Stack.Navigator 
                 screenOptions={{headerShown:false}}
             >
+                <Stack.Screen name="SplashScreen" component={SplashScreen}></Stack.Screen>
                 <Stack.Screen name="LoginScreen" component={LoginScreen}></Stack.Screen>
                 <Stack.Screen name="RegisterScreen" component={RegisterScreen}></Stack.Screen>
                 <Stack.Screen name="HomeScreen" component={HomeScreen}></Stack.Screen>
